08d79f1244bb5b29689f449c07f71f3e4525b3a5
EslamKhder <eslamkhder81@gmail.com>
2020-11-15 01:22:16 +0200

    Java FX project and login part 1 #1

---
+++ src/javafxapplication/JavaFXApplication.java
@@ -0,0 +1,17 @@
+package javafxapplication;
+
+/**
+ *
+ * @author MBR
+ */
+public class JavaFXApplication {
+  
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
