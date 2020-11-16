6a57db7e87202815d94661fa6368c1b55aa7819f
EslamKhder <eslamkhder81@gmail.com>
2020-11-16 21:20:11 +0200

    add loading gift

--- src/javafxapplication/JavaFXApplication.java
+++ src/javafxapplication/JavaFXApplication.java
@@ -1,17 +1,13 @@
 package javafxapplication;
-
 /**
  *
  * @author MBR
  */
 public class JavaFXApplication {
-  
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
     }
-    
 }
