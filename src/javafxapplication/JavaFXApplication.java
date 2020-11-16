a28ddcf61f2e6885cb9a461c262ae8f8295ed6b4
EslamKhder <eslamkhder81@gmail.com>
2020-11-16 21:48:36 +0200

    Implementation Main and test Project  Closes #4

--- src/javafxapplication/JavaFXApplication.java
+++ src/javafxapplication/JavaFXApplication.java
@@ -1,13 +1,29 @@
 package javafxapplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author MBR
  */
-public class JavaFXApplication {
+public class JavaFXApplication extends Application{
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/frontend/Register.fxml"));
+        Scene scene = new Scene(root,600,500);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
